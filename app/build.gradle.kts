232146610f9b1878113bf0b274f893a750278160
Emmalee Gomez <gomeze@bu.edu>
2024-04-03 01:31:17 -0400

    added splash screen and basic navigation bar to fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -39,6 +44,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/bignerdranch/android/aye_ideacanvas/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/aye_ideacanvas/MainActivity.kt
@@ -2,10 +2,51 @@ package com.bignerdranch.android.aye_ideacanvas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.aye_ideacanvas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        splashScreen()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.create -> replaceFragment(CreateFragment())
+                R.id.notifications -> replaceFragment(NotificationsFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+            }
+
+            true
+
+        }
+
     }
+
+    private fun splashScreen(){
+        Thread.sleep(1500)
+        installSplashScreen()
+    }
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
