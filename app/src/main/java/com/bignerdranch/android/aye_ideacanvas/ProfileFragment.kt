0dc9513717c2c0d629cec5c32087346aba4614af
Emmalee Gomez <gomeze@bu.edu>
2024-04-03 01:33:16 -0400

    add spacing

--- app/src/main/java/com/bignerdranch/android/aye_ideacanvas/ProfileFragment.kt
+++ app/src/main/java/com/bignerdranch/android/aye_ideacanvas/ProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/bignerdranch/android/aye_ideacanvas/SearchFragment.kt
+++ app/src/main/java/com/bignerdranch/android/aye_ideacanvas/SearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 class SearchFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
