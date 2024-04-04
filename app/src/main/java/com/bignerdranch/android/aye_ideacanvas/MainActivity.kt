2612defcce47acb7993a9f182f92f18c529326f6
Emmalee Gomez <gomeze@bu.edu>
2024-04-04 14:11:05 -0400

    programatically styled the navigation bar

--- app/src/main/java/com/bignerdranch/android/aye_ideacanvas/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/aye_ideacanvas/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.aye_ideacanvas
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -7,12 +9,14 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.bignerdranch.android.aye_ideacanvas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
+
     private val binding
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
@@ -27,16 +31,36 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        val lightItemTextColorStateList = createColorStateList(
+            R.color.gray, R.color.black, R.color.black
+        )
+
+        val darkItemTextColorStateList = createColorStateList(
+            R.color.gray, R.color.white, R.color.white
+        )
+
         val navHomeFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
         navController = navHomeFragment.navController
+
+        var itemTextColorStateList : ColorStateList
 
         setupWithNavController(binding.bottomNavigationView, navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.create || destination.id == R.id.createDetails) {
                 binding.bottomNavigationView.visibility = View.GONE
+
             } else {
                 binding.bottomNavigationView.visibility = View.VISIBLE
+                if (destination.id == R.id.home) {
+                    itemTextColorStateList = darkItemTextColorStateList
+                    binding.bottomNavigationView.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
+                } else {
+                    itemTextColorStateList = lightItemTextColorStateList
+                    binding.bottomNavigationView.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                }
+                binding.bottomNavigationView.itemTextColor = itemTextColorStateList
+                binding.bottomNavigationView.itemIconTintList = itemTextColorStateList
             }
         }
     }
@@ -44,6 +68,21 @@ class MainActivity : AppCompatActivity() {
     private fun splashScreen(){
         Thread.sleep(1500)
         installSplashScreen()
+    }
+
+    private fun createColorStateList(defaultColorResId: Int, pressedColorResId: Int, checkedColorResId: Int): ColorStateList {
+        val defaultColor = ContextCompat.getColor(this, defaultColorResId)
+        val pressedColor = ContextCompat.getColor(this, pressedColorResId)
+        val checkedColor = ContextCompat.getColor(this, checkedColorResId)
+
+        val states = arrayOf(
+            intArrayOf(android.R.attr.state_pressed),
+            intArrayOf(android.R.attr.state_checked),
+            intArrayOf()
+        )
+
+        val colors = intArrayOf(pressedColor, checkedColor, defaultColor)
+        return ColorStateList(states, colors)
     }
 
     override fun onDestroy() {
