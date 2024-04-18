3029fc8344c892973a82102f38adee380598ecc1
Elliot Kim <3lliotkim@gmail.com>
2024-04-18 13:44:56 -0400

    InsertPost function

--- Aye-IdeaCanvasV2/app/src/main/java/com/android/aye_ideacanvasv2/IdeaCanvasDB.kt
+++ Aye-IdeaCanvasV2/app/src/main/java/com/android/aye_ideacanvasv2/IdeaCanvasDB.kt
@@ -71,4 +71,38 @@ object IdeaCanvasDB {
 
         return dataItem
     }
+
+    suspend fun insertPost(postId: Int, userId:Int, title: String, imageUrl: String?, content: String?) {
+        withContext(Dispatchers.IO) {
+            try {
+                Class.forName("org.postgresql.Driver")
+
+                val conn: Connection = DriverManager.getConnection(
+                    "jdbc:postgresql://aye-ideacanvas-14031.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=require",
+                    "elliot",
+                    "hPX6uyZJIyfoqHQJpGwLMg"
+                )
+
+                val statement = conn.prepareStatement("INSERT INTO post_table(post_id, user_id title, image_url, content) VALUES (?, ?, ?, ?, ?)")
+                statement.setInt(1, postId)
+                statement.setInt(2, userId)
+                statement.setString(3, title)
+                statement.setString(4, imageUrl)
+                statement.setString(5, content)
+                statement.executeUpdate()
+
+                conn.close()
+
+                Log.d("Database Action", "Data inserted successfully")
+            } catch (e: SQLException) {
+                e.printStackTrace()
+                Log.d("Database Action", "Failed to insert data")
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+                Log.d("Database Action", "Failed to load JDBC driver")
+            }
+        }
+    }
+
+
 }
