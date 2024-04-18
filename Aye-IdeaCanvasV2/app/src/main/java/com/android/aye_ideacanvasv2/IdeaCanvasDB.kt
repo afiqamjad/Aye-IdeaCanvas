b8caba300d62837ac6747d96d06c7f61feb2da48
Elliot Kim <3lliotkim@gmail.com>
2024-04-18 14:27:48 -0400

    Added fetchPost & updated db handling of sensitive info

--- Aye-IdeaCanvasV2/app/src/main/java/com/android/aye_ideacanvasv2/IdeaCanvasDB.kt
+++ Aye-IdeaCanvasV2/app/src/main/java/com/android/aye_ideacanvasv2/IdeaCanvasDB.kt
@@ -1,22 +1,45 @@
 package com.android.aye_ideacanvasv2
 
 import android.util.Log
+import com.android.aye_ideacanvasv2.model.Post
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
+import java.io.FileInputStream
+import java.util.Properties
 
 object IdeaCanvasDB {
+
+    private val props = Properties()
+
+    init {
+        loadConfig()
+    }
+
+    private fun loadConfig() {
+        try {
+            val configFile = "path/to/config.properties"
+            props.load(FileInputStream(configFile))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private val dbUrl: String = props.getProperty("db.url")
+    private val dbUsername: String = props.getProperty("db.username")
+    private val dbPassword: String = props.getProperty("db.password")
+
     suspend fun insertData(data: String, whichTable: String) {
         withContext(Dispatchers.IO) {
             try {
                 Class.forName("org.postgresql.Driver")
 
                 val conn: Connection = DriverManager.getConnection(
-                    "jdbc:postgresql://aye-ideacanvas-14031.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=require",
-                    "elliot",
-                    "<YOUR_DB_PASSWORD>"
+                    dbUrl,
+                    dbUsername,
+                    dbPassword
                 )
 
                 val statement = conn.prepareStatement("INSERT INTO $whichTable($data) VALUES (?)")
@@ -44,9 +67,9 @@ object IdeaCanvasDB {
                 Class.forName("org.postgresql.Driver")
 
                 val conn = DriverManager.getConnection(
-                    "jdbc:postgresql://aye-ideacanvas-14031.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=require",
-                    "elliot",
-                    "<YOUR_DB_PASSWORD>"
+                    dbUrl,
+                    dbUsername,
+                    dbPassword
                 )
 
                 val statement = conn.prepareStatement("SELECT $name FROM $whichTable WHERE id = '$id';")
@@ -72,19 +95,19 @@ object IdeaCanvasDB {
         return dataItem
     }
 
-    suspend fun insertPost(postId: Int, userId:Int, title: String, imageUrl: String?, content: String?) {
+    suspend fun insertPost(postId: Long, userId:Int, title: String, imageUrl: String?, content: String?) {
         withContext(Dispatchers.IO) {
             try {
                 Class.forName("org.postgresql.Driver")
 
                 val conn: Connection = DriverManager.getConnection(
-                    "jdbc:postgresql://aye-ideacanvas-14031.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=require",
-                    "elliot",
-                    "hPX6uyZJIyfoqHQJpGwLMg"
+                    dbUrl,
+                    dbUsername,
+                    dbPassword
                 )
 
-                val statement = conn.prepareStatement("INSERT INTO post_table(post_id, user_id title, image_url, content) VALUES (?, ?, ?, ?, ?)")
-                statement.setInt(1, postId)
+                val statement = conn.prepareStatement("INSERT INTO post_table(post_id, user_id, title, image_url, content) VALUES (?, ?, ?, ?, ?)")
+                statement.setLong(1, postId)
                 statement.setInt(2, userId)
                 statement.setString(3, title)
                 statement.setString(4, imageUrl)
@@ -102,6 +125,49 @@ object IdeaCanvasDB {
                 Log.d("Database Action", "Failed to load JDBC driver")
             }
         }
+    }
+
+    suspend fun fetchPost(postId: Long): Post? {
+        var post: Post? = null
+
+        withContext(Dispatchers.IO) {
+            try {
+                Class.forName("org.postgresql.Driver")
+
+                val conn = DriverManager.getConnection(
+                    dbUrl,
+                    dbUsername,
+                    dbPassword
+                )
+
+                val statement = conn.prepareStatement("SELECT * FROM post_table WHERE post_id = ?")
+                statement.setLong(1, postId)
+                val resultSet = statement.executeQuery()
+
+                if (resultSet.next()) {
+                    //val userId = resultSet.getInt("user_id")
+                    val username = resultSet.getString("username")
+                    //val title = resultSet.getString("title")
+                    val imageUrl = resultSet.getString("image_url")
+                    val content = resultSet.getString("content")
+
+                    post = Post(username, imageUrl, content)
+                    Log.d("Database Action", "Post fetched successfully")
+                } else {
+                    Log.d("Database Action", "No post found with postId: $postId")
+                }
+
+                conn.close()
+            } catch (e: SQLException) {
+                e.printStackTrace()
+                Log.d("Database Action", "Failed to fetch post")
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+                Log.d("Database Action", "Failed to load JDBC driver")
+            }
+        }
+
+        return post
     }
 
 
